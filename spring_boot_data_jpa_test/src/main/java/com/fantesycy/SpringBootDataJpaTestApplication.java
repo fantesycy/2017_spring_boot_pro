a7553a01a59db06a8a04a2ed5262127004741751
fantesycy <fantestct@163.com>
2017-07-11 23:16:36 +0800

    init

---
+++ spring_boot_data_jpa_test/src/main/java/com/fantesycy/SpringBootDataJpaTestApplication.java
@@ -0,0 +1,12 @@
+package com.fantesycy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDataJpaTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDataJpaTestApplication.class, args);
+	}
+}
